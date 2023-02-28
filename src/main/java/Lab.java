30ee1e8e43f99917085efa544aca307d3e987c65
sushmishra87 <sm.sushmimishra@gmail.com>
2023-02-28 01:22:38 +0000

    Completed Lab Exception Test

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -20,7 +20,8 @@ public class Lab {
      *
      * Check out this guide on throwing exceptions: https://rollbar.com/guides/java/how-to-throw-exceptions-in-java/#
      */
-    public void mustThrow() {
-
+    public void mustThrow() throws Exception
+    {
+            throw new Exception("Exception message");
     }
 }
